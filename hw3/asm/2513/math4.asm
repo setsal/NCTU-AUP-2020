16f4d4dfefe96bf96a6c290863bfa76336a8a2d6
setsal Lan <contact@setsal.dev>
2021-06-29 14:57:13 +0800

    update

--- hw3/asm/2513/math4.asm
+++ hw3/asm/2513/math4.asm
@@ -1,11 +1,12 @@
-; ➜ nc aup.zoolab.org 2514
+; ➜ nc aup.zoolab.org 2513
 ; math5: 32-bit signed arithmetic
-;         var3 = (var1 * -var2) / (var3 - ebx)
-;         note: overflowed part should be truncated
+;     var4 = (var1 * -5) / (-var2 % var3)
+;     note: overflowed part should be truncated
 ; ======
 ;       var1 @ 0x600000-600004
 ;       var2 @ 0x600004-600008
 ;       var3 @ 0x600008-60000c
+;       var4 @ 0x60000c-600010
 ; ======
 
 
@@ -17,10 +18,10 @@ neg eax
 mov ecx, eax
 mov eax, [0x600004]
 neg eax
-mov ebx, [0x600008]
+mov ebx, [0x600008]  ;idiv dword ptr[0x600008]	
 cdq
 idiv ebx
-mov ebx, edx
+mov ebx, edx ; remider in edx
 
 mov eax, ecx
 cdq
--- hw3/asm/2514/math5.asm
+++ hw3/asm/2514/math5.asm
@@ -11,11 +11,11 @@
 mov eax, [0x600000]
 mov ecx, [0x600004]
 neg ecx
-imul ecx
+imul ecx    ;result in edx:eax
 
 mov ecx, [0x600008]
 sub ecx, ebx
-xor edx, edx
+xor edx, edx    ; strip value
 idiv ecx
 
 mov [0x600008], eax
